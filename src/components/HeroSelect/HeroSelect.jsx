0f38fd096adba0501c0dee4502844520e8aa686c
tiago1820 <tiago.zdo@gmail.com>
2024-01-08 16:46:28 -0300

    Navbar and Header done.

--- src/components/HeroSelect/HeroSelect.jsx
+++ src/components/HeroSelect/HeroSelect.jsx
@@ -2,12 +2,12 @@ import styles from "./HeroSelect.module.css";
 
 export const HeroSelect = () => {
     return (
-        <div className={`container-fluid ${styles.heroSelect}`}>
+        <div className="container-fluid">
             <div className="row">
-                <div className={`col-md-6 col-sm-12 ${styles.selectContainer}`}>
+                <div className="col-md-6 col-sm-12">
                     <div class="input-group mb-3">
                         <label class="input-group-text" for="inputGroupSelect01">¿Cuál es su industria?</label>
-                        <select class="form-select" id="inputGroupSelect01">
+                        <select class={`form-select ${styles.select}`} id="inputGroupSelect01">
                             <option value="1">Alimentos y bebidas</option>
                             <option value="2">Textil y calzado</option>
                             <option value="3">Manufactura</option>
